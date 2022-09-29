39349534a33b4b2060a6230d79c7d1cdd01e14b5
Espen Hagen <2492641+espenhgn@users.noreply.github.com>
2022-09-29 13:28:55 +0000

    run ./configure w. bcftools

--- src/scripts/install_bcftools.sh
+++ src/scripts/install_bcftools.sh
@@ -3,8 +3,9 @@
 set -e
 
 # deps 
-apt-get update && apt-get install -y --no-install-recommends && \
-    curl libcurl4-gnutls-dev libbz2-dev liblzma-dev && \
+apt-get update && apt-get install -y --no-install-recommends \
+    curl libcurl4-gnutls-dev libbz2-dev liblzma-dev \
+    libperl-dev && \
     rm -rf /var/lib/apt/lists/*
 
 
@@ -12,7 +13,7 @@ apt-get update && apt-get install -y --no-install-recommends && \
 git clone --recurse-submodules --depth 1 -b 1.12 https://github.com/samtools/htslib.git && \
 git clone --recurse-submodules --depth 1 -b 1.12 https://github.com/samtools/bcftools.git && \
 cd bcftools && \
-#autoheader && autoconf && ./configure --enable-libgsl --enable-perl-filters && \
+autoheader && autoconf && ./configure --enable-libgsl --enable-perl-filters && \
 make -j12 && \
 make install
 
