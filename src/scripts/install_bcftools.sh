24bdf3dea9cbf6c9124efaf3ad0a88e9ef4776a1
Espen Hagen <2492641+espenhgn@users.noreply.github.com>
2022-09-29 12:47:37 +0000

    fixed failure to clone repos

--- src/scripts/install_bcftools.sh
+++ src/scripts/install_bcftools.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 
+set -e
+
+# deps 
+apt-get update && apt-get install -y --no-install-recommends && \
+    curl libcurl4-gnutls-dev libbz2-dev liblzma-dev && \
+    rm -rf /var/lib/apt/lists/*
+
 
 # bcfools
-git clone --recurse-submodules --depth 1 --branch 1.12 git://github.com/samtools/htslib.git && \
-git clone --recurse-submodules --depth 1 --branch 1.12 git://github.com/samtools/bcftools.git && \
+git clone --recurse-submodules --depth 1 -b 1.12 https://github.com/samtools/htslib.git && \
+git clone --recurse-submodules --depth 1 -b 1.12 https://github.com/samtools/bcftools.git && \
 cd bcftools && \
-apt-get update && apt-get install -y curl libcurl4-gnutls-dev libbz2-dev liblzma-dev && \
 #autoheader && autoconf && ./configure --enable-libgsl --enable-perl-filters && \
-make -j12
+make -j12 && \
 make install
 
 #cp /bcftools/bcftools  /bin
