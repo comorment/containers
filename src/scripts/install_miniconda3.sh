5abd6aa9afad5b4c5359a71e735459b3a57a9422
Espen Hagen <2492641+espenhgn@users.noreply.github.com>
2022-09-30 13:53:23 +0000

    set channel priority

--- src/scripts/install_miniconda3.sh
+++ src/scripts/install_miniconda3.sh
@@ -4,3 +4,12 @@ curl -sSL https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh 
   && rm -rf /tmp/miniconda3.sh
 
 export PATH=$PATH:/opt/conda/bin
+
+# stop complaints about outdated conda version
+# conda update -n base -c defaults conda
+
+# use correct channel ordering for bioconda
+conda config --add channels defaults
+conda config --add channels bioconda
+conda config --add channels conda-forge
+conda config --set channel_priority strict
