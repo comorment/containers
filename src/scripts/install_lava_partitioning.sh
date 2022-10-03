18d51ec262d4fb9ac3f1b6b6e8c04f1dba991c5e
Espen Hagen <2492641+espenhgn@users.noreply.github.com>
2022-10-03 08:27:50 +0000

    remove LAVA and lava_partitioning from r.sif

--- src/scripts/install_lava_partitioning.sh
+++
@@ -1,6 +0,0 @@
-# the original software is https://github.com/cadeleeuw/lava-partitioning
-# ofrei fork is a minor change in the makefile 
-git clone https://github.com/ofrei/lava-partitioning.git && cd lava-partitioning
-make
-cp ldblock /bin
-
