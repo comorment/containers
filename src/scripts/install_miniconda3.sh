87c2c0b83fccf6d645dde92d88769c3bbe8301a1
Espen Hagen <2492641+espenhgn@users.noreply.github.com>
2022-10-03 14:10:09 +0000

    download miniforge3

--- src/scripts/install_miniconda3.sh
+++ src/scripts/install_miniconda3.sh
@@ -1,7 +1,7 @@
-curl -sSL https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -o /tmp/miniconda3.sh \
+curl -sSL https://github.com/conda-forge/miniforge/releases/download/4.14.0-0/Miniforge3-4.14.0-0-Linux-x86_64.sh -o /tmp/miniforge3.sh \
   && mkdir /root/.conda \
-  && bash /tmp/miniconda3.sh -bfp /usr/local \
-  && rm -rf /tmp/miniconda3.sh
+  && bash /tmp/miniforge3.sh -bfp /usr/local \
+  && rm -rf /tmp/miniforge3.sh
 
 export PATH=$PATH:/opt/conda/bin
 
@@ -9,7 +9,7 @@ export PATH=$PATH:/opt/conda/bin
 # conda update -n base -c defaults conda
 
 # use correct channel ordering for bioconda
-conda config --add channels defaults
-conda config --add channels bioconda
-conda config --add channels conda-forge
-conda config --set channel_priority strict
+# conda config --add channels defaults
+# conda config --add channels bioconda
+# conda config --add channels conda-forge
+# conda config --set channel_priority strict
