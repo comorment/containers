67f1361878016829d02631b07ab0af9dde50d712
Espen Hagen <2492641+espenhgn@users.noreply.github.com>
2022-10-04 11:28:03 +0000

    use mamba; mamba clean & pip cache purge

--- src/scripts/install_miniforge3.sh
+++ src/scripts/install_miniforge3.sh
@@ -1,4 +1,4 @@
-curl -sSL https://github.com/conda-forge/miniforge/releases/download/4.14.0-0/Miniforge3-4.14.0-0-$(uname)-$(uname -m).sh -o /tmp/miniforge3.sh \
+curl -sSL https://github.com/conda-forge/miniforge/releases/download/4.14.0-0/Mambaforge-4.14.0-0-$(uname)-$(uname -m).sh -o /tmp/miniforge3.sh \
   && mkdir /root/.conda \
   && bash /tmp/miniforge3.sh -bfp /usr/local \
   && rm -rf /tmp/miniforge3.sh
