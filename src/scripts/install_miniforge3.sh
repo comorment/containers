1fdc4fa47b59710a8f10fa4bc30f7bd96311edfa
Espen Hagen <2492641+espenhgn@users.noreply.github.com>
2022-10-04 09:02:01 +0000

    removed comments

--- src/scripts/install_miniforge3.sh
+++ src/scripts/install_miniforge3.sh
@@ -4,13 +4,3 @@ curl -sSL https://github.com/conda-forge/miniforge/releases/download/4.14.0-0/Mi
   && rm -rf /tmp/miniforge3.sh
 
 export PATH=$PATH:/opt/conda/bin
-
-# stop complaints about outdated conda version
-# conda update -n base -c defaults conda
-
-# use correct channel ordering for bioconda
-# conda config --add channels defaults
-# conda config --add channels bioconda
-# conda config --add channels conda-forge
-# conda config --set channel_priority strict
-
