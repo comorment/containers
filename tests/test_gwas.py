c78684a7ac11a9b80fc8c8223288c02062f7cc2b
Espen Hagen <2492641+espenhgn@users.noreply.github.com>
2022-10-04 13:11:13 +0000

    add test that Minimac4 is installed in gwas.sif

--- tests/test_gwas.py
+++ tests/test_gwas.py
@@ -7,13 +7,19 @@ Test module for ``gwas.sif`` build
 import os
 import subprocess
 
+pth = os.path.join('singularity', 'gwas.sif')
 
-def test_gwas():
-    pth = os.path.join('singularity', 'gwas.sif')
+def test_plink():
     call = f'singularity run {pth} plink --version'
     out = subprocess.run(call.split(' '))
     assert out.returncode == 0
 
+def test_plink2()
     call = f'singularity run {pth} plink2 --version'
+    out = subprocess.run(call.split(' '))
+    assert out.returncode == 0
+
+def test_minimac4():
+    call = f'singularity run {pth} minimac4 --version'
     out = subprocess.run(call.split(' '))
     assert out.returncode == 0
