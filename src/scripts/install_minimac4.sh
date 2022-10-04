8452183d4820815c4835ae341d7679b9c55b256d
Espen Hagen <2492641+espenhgn@users.noreply.github.com>
2022-10-04 13:10:55 +0000

    revised Minimac4 installer script

--- src/scripts/install_minimac4.sh
+++ src/scripts/install_minimac4.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
 
-apt-get update
-apt-get install cmake -y python-pip python-dev -y
-pip install cget
+apt-get update && \
+    apt-get install --no-install-recommends \
+    python-pip python-setuptools python-click python-six python-dev -y && \
+    rm -rf /var/lib/apt/lists/*
+
+pip install --no-cache-dir cget
 git clone https://github.com/statgen/Minimac4.git && \
 cd Minimac4 && \
 cget install -f ./requirements.txt && \
@@ -13,3 +16,8 @@ make && \
 cp minimac4 /bin
 #make installi
 
+# remove cget, python-pip etc. used to build minimac4
+pip uninstall cget
+apt-get purge \
+    python-setuptools python-pip python-dev python-click python-six -y && \
+    apt-get autoremove --purge -y
