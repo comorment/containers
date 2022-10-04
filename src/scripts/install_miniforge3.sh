042e5ea7d2e763e2e2d15303c2cf8fac47e1c3d8
Espen Hagen <2492641+espenhgn@users.noreply.github.com>
2022-10-04 11:43:35 +0000

    use Mambaforge

--- src/scripts/install_miniforge3.sh
+++
@@ -1,6 +0,0 @@
-curl -sSL https://github.com/conda-forge/miniforge/releases/download/4.14.0-0/Mambaforge-4.14.0-0-$(uname)-$(uname -m).sh -o /tmp/miniforge3.sh \
-  && mkdir /root/.conda \
-  && bash /tmp/miniforge3.sh -bfp /usr/local \
-  && rm -rf /tmp/miniforge3.sh
-
-export PATH=$PATH:/opt/conda/bin
