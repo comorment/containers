b277afd5534e560272add76e2391c27299b5648a
Espen Hagen <2492641+espenhgn@users.noreply.github.com>
2022-10-06 09:46:59 +0000

    pin vcftools@581c23199

--- src/scripts/install_vcftools.sh
+++ src/scripts/install_vcftools.sh
@@ -2,13 +2,14 @@
 
 
 # vcfools
- apt-get update
- apt-get install -y automake pkg-config zlib1g-dev
- git clone https://github.com/vcftools/vcftools.git . && \
- ./autogen.sh && \
- ./configure && \
-  make -j6 && \
-  make install
+apt-get update
+apt-get install -y automake pkg-config zlib1g-dev
+git clone --depth 1 https://github.com/vcftools/vcftools.git . && \
+    git reset --hard 581c231991cb4db017b92eabc573e17128541ab5 && \
+    ./autogen.sh && \
+    ./configure && \
+make -j6 && \
+make install
 
 
 #cp vcftools  /bin
