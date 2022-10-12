46fb42aa37f2f1b8878bd8a56642b14277201237
Espen Hagen <2492641+espenhgn@users.noreply.github.com>
2022-10-12 14:54:15 +0000

    added test for installed R packages in r.sif

--- tests/test_r.py
+++ tests/test_r.py
@@ -19,3 +19,9 @@ def test_r_Rscript():
     call = f'singularity run {pth} Rscript --version'
     out = subprocess.run(call.split(' '))
     assert out.returncode == 0
+
+def test_r_R_packages():
+    pwd = os.getcwd()
+    call = f'singularity run --home={pwd} {pth} Rscript {pwd}/tests/extras/r.R'
+    out = subprocess.run(call.split(' '))
+    assert out.returncode == 0
