8cdbf5b21309bcddcd30bd1c33d7c317df5b1529
Espen Hagen <2492641+espenhgn@users.noreply.github.com>
2022-10-12 15:15:51 +0000

    test for working SAIGE scripts

--- tests/test_saige.py
+++ tests/test_saige.py
@@ -19,3 +19,15 @@ def test_saige_Rscript():
     call = f'singularity run {pth} Rscript --version'
     out = subprocess.run(call.split(' '))
     assert out.returncode == 0
+
+def test_saige_SAIGE():
+    scripts = [
+        'createSparseGRM.R --help',
+        'step1_fitNULLGLMM.R --help',
+        'step2_SPAtests.R --help'
+    ]
+    pwd = os.getcwd()
+    for script in scripts:
+        call = f'singularity exec --home {pwd}:/home {pth} {script}'
+        out = subprocess.run(call.split(' '))
+        assert out.returncode == 0
