20eb770b9ae16b3353c865fb175c46b903068a3a
Espen Hagen <2492641+espenhgn@users.noreply.github.com>
2022-10-24 20:22:16 +0000

    autopep8

--- tests/test_python3.py
+++ tests/test_python3.py
@@ -10,15 +10,18 @@ import subprocess
 
 pth = os.path.join('singularity', 'python3.sif')
 
+
 def test_python3_plink():
     call = f'singularity run {pth} plink --version'
     out = subprocess.run(call.split(' '))
     assert out.returncode == 0
 
+
 def test_python3_python():
     call = f'singularity run {pth} python --version'
     out = subprocess.run(call.split(' '))
     assert out.returncode == 0
+
 
 def test_python3_python_convert():
     args = ['/tools/python_convert/sumstats.py -h',
@@ -29,16 +32,18 @@ def test_python3_python_convert():
         out = subprocess.run(call.split(' '))
         assert out.returncode == 0
 
+
 def test_python3_ukb():
     arg = '/tools/ukb/ukb_helper.py -h'
     call = f'singularity run {pth} python {arg}'
     out = subprocess.run(call.split(' '))
     assert out.returncode == 0
 
+
 def test_python3_packages():
     packages = [
-        'h5py', 
-        'ldpred', 
+        'h5py',
+        'ldpred',
         'lifelines',
         'matplotlib',
         'matplotlib_venn',
