58d4d908512cb9748afb5c199506aac118f8bb98
Espen Hagen <2492641+espenhgn@users.noreply.github.com>
2022-11-02 10:46:47 +0000

    remaining stuff in /tools/ removed

--- src/scripts/install_bolt.sh
+++ src/scripts/install_bolt.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
-
 export VERSION="v2.4"
 wget --no-check-certificate https://storage.googleapis.com/broad-alkesgroup-public/BOLT-LMM/downloads/BOLT-LMM_$VERSION.tar.gz && \
 tar -xvzf BOLT-LMM_$VERSION.tar.gz # && \
 rm -rf BOLT-LMM_$VERSION.tar.gz # && \
 mv BOLT-LMM_$VERSION/* . # && \
-# rmdir BOLT-LMM_$VERSION
 
 # delete some stuff that should not be containerized:
+rm -rf BOLT-LMM_$VERSION
+rm license.txt
+rm BOLT-LMM_$VERSION_manual.pdf
+rm bolt.Makefile.patch
 rm -rf src
 rm -rf example
 rm -rf tables 
