3420a2609f94af77a3d126937c02179e5768f0da
Espen Hagen <2492641+espenhgn@users.noreply.github.com>
2022-11-02 16:10:51 +0000

    BOLT-LMM v2.3.5->v2.4

--- src/scripts/install_vcftools.sh
+++ src/scripts/install_vcftools.sh
@@ -2,7 +2,8 @@
 
 
 # vcfools
-git clone --depth 1 https://github.com/vcftools/vcftools.git . && \
+git clone --depth 1 https://github.com/vcftools/vcftools.git && \
+    cd vcftools && \
     git reset --hard 581c231991cb4db017b92eabc573e17128541ab5 && \
     ./autogen.sh && \
     ./configure && \
