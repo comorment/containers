7da31dd5be3448c87c6b7acd0b0cd10f5515f50b
Espen Hagen <2492641+espenhgn@users.noreply.github.com>
2022-11-04 12:50:57 +0000

    BOLT-LMM  2.4->2.3.5. Cleanup.

--- src/scripts/install_bolt.sh
+++ src/scripts/install_bolt.sh
@@ -1,25 +1,20 @@
 #!/bin/sh
 
-export VERSION="v2.4"
-wget --no-check-certificate https://storage.googleapis.com/broad-alkesgroup-public/BOLT-LMM/downloads/BOLT-LMM_$VERSION.tar.gz && \
-tar -xvzf BOLT-LMM_$VERSION.tar.gz # && \
-rm -rf BOLT-LMM_$VERSION.tar.gz # && \
-mv BOLT-LMM_$VERSION/* . # && \
+export VERSION="v2.3.5"
+                           
+wget --no-check-certificate https://storage.googleapis.com/broad-alkesgroup-public/BOLT-LMM/downloads/old/BOLT-LMM_$VERSION.tar.gz
+tar -xvzf BOLT-LMM_$VERSION.tar.gz
+rm -rf BOLT-LMM_$VERSION.tar.gz
+mv BOLT-LMM_$VERSION/* .
 
 # delete some stuff that should not be containerized:
 rm -rf BOLT-LMM_$VERSION
 rm license.txt
-rm BOLT-LMM_$VERSION_manual.pdf
-rm bolt.Makefile.patch
+rm BOLT-LMM_*_manual.pdf
 rm -rf src
 rm -rf example
 rm -rf tables 
 
-# NB! Bolt-LMM must be executed from /tools/bolt/bolt because it links to binaties in /tools/bolt/lib folder.
-
-# mv bolt.Makefile.patch src/
-# cd src
-# patch Makefile < bolt.Makefile.patch
-# make -d
+# NB! Bolt-LMM must be executed from /tools/bolt/bolt because it links to binaries in /tools/bolt/lib folder.
 
 
