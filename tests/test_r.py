01e3bbb2bd633a4b41c5f2b4bf70ef37c8b13572
Espen Hagen <2492641+espenhgn@users.noreply.github.com>
2022-11-14 21:49:21 +0000

    add silly test that rmarkdown works

--- tests/test_r.py
+++ tests/test_r.py
@@ -6,22 +6,40 @@ Test module for ``r.sif`` build
 
 import os
 import subprocess
+import tempfile
 
 
 pth = os.path.join('singularity', 'r.sif')
+
 
 def test_r_R():
     call = f'singularity run {pth} R --version'
     out = subprocess.run(call.split(' '))
     assert out.returncode == 0
 
+
 def test_r_Rscript():
     call = f'singularity run {pth} Rscript --version'
     out = subprocess.run(call.split(' '))
     assert out.returncode == 0
+
 
 def test_r_R_packages():
     pwd = os.getcwd()
     call = f'singularity run --home={pwd} {pth} Rscript {pwd}/tests/extras/r.R'
     out = subprocess.run(call.split(' '))
     assert out.returncode == 0
+
+
+def test_r_R_rmarkdown():
+    pwd = os.getcwd()
+    with tempfile.TemporaryDirectory() as d:
+        os.chdir(d)
+        os.system(f"cp {os.path.join(pwd, 'tests', 'extras', 'cars.Rmd')} .")
+        os.system(f"cp {os.path.join(pwd, 'tests', 'extras', 'cars.R')} .")
+        assert os.path.isfile('cars.Rmd')
+        sif = os.path.join(pwd, pth)
+        call = f"""singularity run --home={d} {sif} Rscript cars.R"""
+        out = subprocess.run(call.split(' '))
+        assert out.returncode == 0
+        assert os.path.isfile('cars.pdf')
