a18bf929a7dee22694fec35f44c3e247eaf92a53
Espen Hagen <2492641+espenhgn@users.noreply.github.com>
2022-11-14 10:36:24 +0000

    move R install to R; bumb Rocker 4.0.3->4.0.5

---
+++ src/scripts/R/github.R
@@ -0,0 +1,4 @@
+devtools::install_github('zhanxw/seqminer', ref='142204d1005553ea87e1740ff97f0286291e41f9')
+devtools::install_github('dajiangliu/rareGWAMA', ref='72e962dae19dc07251244f6c33275ada189c2126')
+devtools::install_github('GenomicSEM/GenomicSEM', ref='bcbbaffff5767acfc5c020409a4dc54fbf07876b')
+devtools::install_github('MRCIEU/TwoSampleMR', ref='c174107cfd9ba47cf2f780849a263f37ac472a0e')
