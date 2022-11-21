9133475b290fc976071f3fcae9ce2a84d8456104
Espen Hagen <2492641+espenhgn@users.noreply.github.com>
2022-11-21 09:59:38 +0000

    add some R scripts

---
+++ pgs/generate_covariate.R
@@ -0,0 +1,14 @@
+library(data.table)
+library(argparser, quietly=T)
+
+par <- arg_parser('write generate .covariate file')
+par <- add_argument(par, "file_cov", help=".cov input file")
+par <- add_argument(par, "file_eigenvec", help=".eigenvec input file")
+par <- add_argument(par, "file_covariate", help=".covariate output file")
+parsed <- parse_args(par)
+
+covariate <- fread(parsed$file_cov)
+pcs <- fread(parsed$eigenvec, header=F)
+colnames(pcs) <- c("FID","IID", paste0("PC", 1:6))
+cov <- merge(covariate, pcs)
+fwrite(cov,parsed$covariate, sep="\t")
