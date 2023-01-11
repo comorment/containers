82d39c170055e4639e5509e7af7d7d71130188c5
deepchocolate <andreas@systerdyster.se>
2023-01-11 14:25:35 +0100

    Reorganized tests

--- usecases/LDpred2_example/run.sh
+++ usecases/LDpred2_example/run.sh
@@ -18,6 +18,8 @@ export DIR_REF_LDPRED=$DIR_BASE/ldpred2_ref
 export fileInputGeno=$DIR_BASE/usecases/LDpred2_tutorial/tutorial_data/public-data3.bed
 export fileInputSumStats=$DIR_BASE/usecases/LDpred2_tutorial/tutorial_data/public-data3-sumstats.txt
 export fileOutputSNPR=$DIR_TESTS/data/public-data3.rds
+export fileKeepSNPS=/REF/hapmap3/w_hm3.justrs
+export fileOut=$DIR_TESTS/output/public-data.score
 
 # Create shortcut environment variable for Rscript 
 export RSCRIPT="singularity exec -B $DIR_BASE:$DIR_BASE -B $DIR_REFERENCE:/REF $DIR_SIF/r.sif Rscript"
@@ -25,7 +27,7 @@ export RSCRIPT="singularity exec -B $DIR_BASE:$DIR_BASE -B $DIR_REFERENCE:/REF $
 # The different modes to run
 LDPRED_MODES="inf"
 
-echo "Testing RDS/backingfile creation"
+echo "### Testing RDS/backingfile creation"
 ### These two runs ensure that the backing file is created
 ### correctly wheter the uses specifies .rds or not
 # Passing full name of $fileSNPR
@@ -35,38 +37,12 @@ if [ $? -eq 1 ]; then echo "$dump"; exit; fi
 dump=$( $RSCRIPT $DIR_SCRIPTS/createBackingFile.R $fileInputGeno $(dirname $fileOutputSNPR)/$(basename $fileOutputSNPR) )
 if [ $? -eq 1 ]; then exit; fi
 
-# Run with a genotype file that contains missing data
-#export fileInputGeno=/REF/examples/prsice2/EUR
-#export fileOutputSNPR=$DIR_TESTS/data/EUR.rds
-export fileKeepSNPS=/REF/hapmap3/w_hm3.justrs
-#export fileSumstats=/REF/examples/prsice2/Height.gwas.txt.gz
-export fileOut=$DIR_TESTS/output/public-data.score
-
 dump=$( $RSCRIPT $DIR_SCRIPTS/createBackingFile.R $fileInputGeno $fileOutputSNPR )
 if [ $? -eq 1 ]; then exit; fi
 
-### LD
-# LD estimation --file-keep-snps $fileKeepSNPS
-LDE="$RSCRIPT $DIR_SCRIPTS/calculateLD.R --geno-file $fileOutputSNPR \
-  --sumstats $fileInputSumStats  \
-  --dir-genetic-maps $DIR_TESTS/maps/ \
-  --file-ld-blocks $DIR_TESTS/output/ld/ld-chr@.rds \
-  --file-ld-map $DIR_TESTS/output/ld/map.rds
-"
-$LDE
 
-LDP="$RSCRIPT $DIR_SCRIPTS/ldpred2.R \
-  --ld-file $DIR_TESTS/output/ld/ld-chr@.rds \
-  --ld-meta-file $DIR_TESTS/output/ld/map.rds \
-  --merge-by-rsid \
-  --col-stat beta --col-stat-se beta_se \
-  --col-snp-id rsid --col-chr chr --col-bp pos --col-A1 a0 --col-A2 a1 \
-  --geno-file $fileOutputSNPR --sumstats $fileInputSumStats --out $fileOut.inf"
-$LDP
-exit
-
-# Perform full runs using externally provided LD
-# LDREF with blocks
+echo "### Testing ldpred2.R using externally provided LD"
+# Download data if necessary
 FILE_LDREF=$DIR_TESTS/data/ld/ldref_with_blocks.zip
 FILE_LDMAP=$DIR_TESTS/data/ld/map_hm3_plus.rds
 if [ ! -f $FILE_LDREF ]; then 
@@ -88,18 +64,48 @@ LDP="$RSCRIPT $DIR_SCRIPTS/ldpred2.R --file-keep-snps $fileKeepSNPS \
   --col-snp-id rsid --col-chr chr --col-bp pos --col-A1 a0 --col-A2 a1 \
   --geno-file $fileOutputSNPR --sumstats $fileInputSumStats --out $fileOut.inf"
 
-## TEST: Bad ldpred mode
+echo "TEST error: Bad ldpred mode"
 dump=$( { $LDP --ldpred-mode infinite; } 2>&1 )
-if [ $? -eq 0 ]; then exit; fi
+if [ $? -eq 0 ]; then echo "No error received"; exit; fi
 
-## TEST: Bad imputation mode
+echo "TEST error: Bad imputation mode"
 dump=$( { $LDP --ldpred-mode inf --geno-impute bad-mode; } 2>&1 )
-if [ $? -eq 0 ]; then exit; fi
+if [ $? -eq 0 ]; then echo "No error received"; exit; fi
 
 ## TEST: Complete runs of --ldpred-mode given by $LDPRED_MODES
 for MODE in $LDPRED_MODES; do
  echo "Testing mode $MODE"
  dump=$( { $LDP --ldpred-mode $MODE; } 2>&1 )
  if [ $? -eq 1 ]; then echo "$dump"; exit; fi
- echo "$dump"
 done
+
+echo "### Testing LD estimation with calculateLD.R and use in ldpred2.R"
+# Basic command to perform LD estimation
+LDE="$RSCRIPT $DIR_SCRIPTS/calculateLD.R --geno-file $fileOutputSNPR \
+  --dir-genetic-maps $DIR_TESTS/maps/ \
+  --file-ld-blocks $DIR_TESTS/output/ld/ld-chr@.rds \
+  --file-ld-map $DIR_TESTS/output/ld/map.rds
+"
+
+echo "Test restricting on a subset of SNPs (only chromosome 1-9 will run)"
+fileSumstats25k=$DIR_TESTS/data/public-data-sumstats25k.txt
+head -n 25000 $fileInputSumStats > $fileSumstats25k
+dump=$( { $LDE --sumstats $fileSumstats25k rsid; } 2>&1 )
+if [ $? -eq 1 ]; then echo "$dump"; exit; fi
+
+echo "Test restricting on SNPs provide by a SNP list file: $fileKeepSNPS"
+dump=$( { $LDE --file-keep-snps $fileKeepSNPS; } 2>&1 )
+if [ $? -eq 1 ]; then echo "$dump"; exit; fi
+
+# Test no restrictions on snps (similar to tutorial
+dump=$( { $LDE; } 2>&1 )
+# Use this LD to run LDpred
+LDP="$RSCRIPT $DIR_SCRIPTS/ldpred2.R \
+  --ld-file $DIR_TESTS/output/ld/ld-chr@.rds \
+  --ld-meta-file $DIR_TESTS/output/ld/map.rds \
+  --merge-by-rsid \
+  --col-stat beta --col-stat-se beta_se \
+  --col-snp-id rsid --col-chr chr --col-bp pos --col-A1 a0 --col-A2 a1 \
+  --geno-file $fileOutputSNPR --sumstats $fileInputSumStats --out $fileOut.inf"
+dump=$( { $LDP; } 2>&1 )
+if [ $? -eq 1 ]; then echo "$dump"; exit; fi
