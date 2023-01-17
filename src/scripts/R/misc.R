a117b804f6e7b0d0d0c928db5872ef568f2412eb
Espen Hagen <2492641+espenhgn@users.noreply.github.com>
2023-01-17 09:55:48 +0100

    add some version info

--- src/scripts/R/misc.R
+++ src/scripts/R/misc.R
@@ -1,4 +1,4 @@
 # library(BiocManager)
 install.packages('http://cnsgenomics.com/software/gsmr/static/gsmr_1.0.9.tar.gz', repos=NULL, type='source')
-# version=3.12 refs to the Bioconductor version, installs zlibbioc_1.36.0, BiocGenerics_0.36.1, snpStats_1.40.0:
+# version=3.12 refs to the Bioconductor version, installs zlibbioc_1.36.0, BiocGenerics_0.36.1, snpStats_1.40.0, GWASTools_1.35.2:
 BiocManager::install(c('zlibbioc', 'BiocGenerics', 'snpStats', 'GWASTools'), version='3.12')
