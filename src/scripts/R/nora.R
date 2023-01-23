1eeb666126e5a215c872700bb2aa2d18df9a512b
Espen Hagen <2492641+espenhgn@users.noreply.github.com>
2023-01-23 12:08:07 +0000

    add some packages for Nora PRS

---
+++ src/scripts/R/nora.R
@@ -0,0 +1,6 @@
+devtools::install_github("psychgen/phenotools", ref="b744d927a1302d85152917f3802a2212093d588a")
+devtools::install_github("norment/normentR", ref="dfa1fbae9587db6c3613b0405df4f9cfa98ee0e1")
+# manhattan  # what the hell is this package?
+devtools::install_version('ggtext', version='0.1.2', repos='https://cloud.r-project.org')
+devtools::install_version('janitor', version='2.1.0', repos='https://cloud.r-project.org')
+devtools::install_version('haven', version='2.5.1', repos='https://cloud.r-project.org')
