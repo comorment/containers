b2ab6f5d37003ae6f810c1d952215da490501ecb
Espen Hagen <2492641+espenhgn@users.noreply.github.com>
2023-02-06 08:30:49 +0000

    phenotools 0.2.7

--- src/scripts/R/nora.R
+++ src/scripts/R/nora.R
@@ -1,6 +1,3 @@
-devtools::install_github("psychgen/phenotools", ref="b744d927a1302d85152917f3802a2212093d588a")
-devtools::install_github("norment/normentR", ref="dfa1fbae9587db6c3613b0405df4f9cfa98ee0e1")
-# manhattan  # what the hell is this package?
 devtools::install_version('ggtext', version='0.1.2', repos='https://cloud.r-project.org')
 devtools::install_version('janitor', version='2.1.0', repos='https://cloud.r-project.org')
 devtools::install_version('haven', version='2.5.1', repos='https://cloud.r-project.org')
@@ -13,3 +10,5 @@ devtools::install_version('ggrepel', version='0.9.2', repos='https://cloud.r-pro
 devtools::install_version('psych', version='2.2.9', repos='https://cloud.r-project.org')
 devtools::install_version('jtools', version='2.2.1', repos='https://cloud.r-project.org')
 devtools::install_version('MplusAutomation', version='1.1.0', repos='https://cloud.r-project.org')
+devtools::install_github("psychgen/phenotools", ref="a602e6513b00f96e33ddff60905c4629f824d44d")
+devtools::install_github("norment/normentR", ref="dfa1fbae9587db6c3613b0405df4f9cfa98ee0e1")
