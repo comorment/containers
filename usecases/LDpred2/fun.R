81b9bf155fab0720889b86c0acd78ad98b9ac064
deepchocolate <andreas@systerdyster.se>
2023-02-23 17:32:58 +0100

    Removed snp_fastImpute

--- usecases/LDpred2/fun.R
+++ usecases/LDpred2/fun.R
@@ -1,15 +1,15 @@
 # Calculate counts of missingness in the genotype matrix, i.e., in the columns of the matrix.
 #' @param genoMat A FBM.code256 bigSNP genotype matrix (eg, <bigSNP>$genotypes)
 #' @return A list of counts of missingness per genotype across all individuals
-countMissingGenotypes <-  function(genoMat) {
+countMissingGenotypes <-  function(genoMat, cores=nb_cores()) {
   require(bigsnpr)
-  big_apply(genoMat, a.FUN=function (x, ind) colSums(is.na(x[,ind])), a.combine='c')
+  big_apply(genoMat, a.FUN=function (x, ind) colSums(is.na(x[,ind])), a.combine='c', ncores = cores)
 }
 
 # Replace missing genotypes with zero
 #' @param genoMat A FBM.code256 bigSNP genotype matrix (eg, <bigSNP>$genotypes)
 #' @return A FBM.code256 bigSNP genotype matrix
 zeroMissingGenotypes <- function(genoMat) {
-  if (class(G) != "FBM.code256") stop('Argument must be a FBM.code256 object. Received ', class(G))
+  if (class(genoMat) != "FBM.code256") stop('Argument must be a FBM.code256 object. Received ', class(genoMat))
   genoMat$copy(code=c(0,1,2, rep(0, 253)))
 }
--- usecases/LDpred2/imputeGenotypes.R
+++ usecases/LDpred2/imputeGenotypes.R
@@ -4,69 +4,37 @@ options(bigstatsr.check.parallel.blas = FALSE)
 options(default.nproc.blas = NULL)
 library(tools)
 library(argparser, quietly=T)
-par <- arg_parser('Impute genotype in bigSNPR files')
+par <- arg_parser('Impute genotype in bigSNPR files. Note that imputation will be performed in the file provided and not in a copy.')
 par <- add_argument(par, '--geno-file-rds', nargs=1, help='Genotype .rds file to impute')
-
 par <- add_argument(par, '--impute-simple', nargs=1, help='Impute missing genotypes using one of mode, mean0, mean2, random (See bigsnpr::fastImputeSimple) or zero')
-par <- add_argument(par, '--impute-alpha', nargs=1, default=1e-4, help='Imputation type-I error threhold, only relevant for --impute')
-par <- add_argument(par, '--impute-size', nargs=1, default=200, help='Imputation number of neighbour SNPs, only relevant for --impute')
-par <- add_argument(par, '--impute-ptrain', nargs=1, default=0.8, help='Proportion missing genotypes used for training, only relevant for --impute')
-par <- add_argument(par, '--impute-seed', nargs=1, help='Provide a seed to bigsnpr::snp_fastImpute for reproducible imputation.')
 par <- add_argument(par, '--cores', nargs=1, default=nb_cores(), help='Number of cores to use.')
 parsed <- parse_args(par)
 fileGeno <- parsed$geno_file_rds
 fileGenoOutput <- parsed$geno_output_file_rds
 imputeSimple <- parsed$impute_simple
 NCORES <- parsed$cores
-argImputeAlpha <- parsed$impute_alpha
-argImputeSize <- parsed$impute_size
-argImputePtrain <- parsed$impute_ptrain
-argImputeSeed <- parsed$impute_seed
 ### Maybe there's some environment variable availble to determine the location of the script instead
 coms <- commandArgs()
-coms <- coms[substr(coms, 1, 8) == '--file=/']
+coms <- coms[substr(coms, 1, 7) == '--file=']
 dirScript <- dirname(substr(coms, 8, nchar(coms)))
 source(paste0(dirScript, '/fun.R'))
 
-
 if (!file.exists(fileGeno)) stop(fileGeno, ' does not exist!')
-
 cat('Loading genotype data from file:', fileGeno ,'\n')
 obj.bigSNP <- snp_attach(fileGeno)
 G <- obj.bigSNP$genotypes
 # Count missingness over individuals for each SNP
-nMissingGenotypes <- countMissingGenotypes(G)
+nMissingGenotypes <- countMissingGenotypes(G, cores=NCORES)
 nWithMissing <- sum(nMissingGenotypes > 0)
 if (nWithMissing == 0) {
   cat('No missing genotypes found. Exiting...\n')
   quit('no')
 }
-cat('Genotypes missing for at least 1 individual: N=', nWithMissing, ' out of ', length(nMissingGenotypes), ').\n', sep='')
-if (!is.na(imputeSimple)) {
-  cat('Imputing genotypes by using', imputeSimple, '(see bigsnpr::snp_fastImputeSimple).\n')
-  if (imputeSimple == 'zero') G <- zeroMissingGenotypes(G)
-  else G <- snp_fastImputeSimple(G, method=imputeSimple, ncores = NCORES)
-} else {
-  library(xgboost)
-  cat('Imputing genotypes by using XGBoost models (see bigsnpr::snp_fastImpute).\n')
-  infos <- snp_fastImpute(G, 
-                          infos.chr=obj.bigSNP$map$chromosome,
-                          alpha=argImputeAlpha, size=argImputeSize, p.train=argImputePtrain, ncores=NCORES,
-                          seed=argImputeSeed)
-  # ggplot2
-  library(ggplot2)
-  ## Here there is no SNP with more than 1% error (estimated)
-  pvals <- c(0.01, 0.005, 0.002, 0.001); colvals <- 2:5
-  df <- data.frame(pNA = infos[1, ], pError = infos[2, ])
-  
-  Reduce(function(p, i) {
-    p + stat_function(fun = function(x) pvals[i] / x, color = colvals[i])
-  }, x = seq_along(pvals), init = ggplot(df, aes(pNA, pError))) +
-    geom_point() +
-    coord_cartesian(ylim = range(df$pError, na.rm = TRUE)) +
-    theme_bigstatsr()
-}
-nMissingGenotypes <- countMissingGenotypes(G)
+cat('Genotypes missing for at least 1 individual: N=', nWithMissing, ' out of ', length(nMissingGenotypes), ' genotypes).\n', sep='')
+cat('Imputing genotypes by using', imputeSimple, '(see bigsnpr::snp_fastImputeSimple).\n')
+if (imputeSimple == 'zero') G <- zeroMissingGenotypes(G);
+else G <- snp_fastImputeSimple(G, method=imputeSimple, ncores = NCORES);
+nMissingGenotypes <- countMissingGenotypes(G, cores=NCORES)
 nWithMissing <- sum(nMissingGenotypes > 0)
 if (nWithMissing > 0) stop('Imputation has failed: Still missing genotypes')
 cat('Storing imputed genotypes in', fileGeno, '\n')
