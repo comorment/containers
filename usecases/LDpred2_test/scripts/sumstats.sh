e5f9501f42a523aec524395bcdb8820dd311b68f
deepchocolate <andreas@systerdyster.se>
2023-04-24 16:55:01 +0200

    Reorganized test run

--- usecases/LDpred2_test/scripts/sumstats.sh
+++ usecases/LDpred2_test/scripts/sumstats.sh
@@ -2,10 +2,10 @@
 fileSumstats=$DIR_TESTS/unittest/data/sumstats.txt
 fileReference=$DIR_TESTS/unittest/data/hrc37.txt
 # The tests are not run using the full HRC data to speed things up
-COM="$RSCRIPT $DIR_SCRIPTS/complementSumstats.R --sumstats $fileSumstats --reference $fileReference"
+COM="$RSCRIPT $DIR_SCRIPTS/complementSumstats.R --reference $fileReference"
 # Output to stdout
-dump=$( { $COM; } 2>&1 )
+dump=$( { $COM --sumstats $fileSumstats; } 2>&1 )
 if [ $? -eq 1 ]; then echo "$dump"; exit; fi
 # Add up som more columns than the defaults
-dump=$( { $COM --columns-append "#CHROM" POS AF; } 2>&1 )
+dump=$( { $COM --columns-append "#CHROM" POS AF --sumstats $fileSumstats ; } 2>&1 )
 if [ $? -eq 1 ]; then echo "$dump"; exit; fi
