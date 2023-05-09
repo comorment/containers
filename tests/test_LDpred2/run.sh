34d1f0d89a03664524bbd921511816b863bb6799
Espen Hagen <2492641+espenhgn@users.noreply.github.com>
2023-05-09 08:15:21 +0000

    update LDpred2 test script

--- tests/test_LDpred2/run.sh
+++ tests/test_LDpred2/run.sh
@@ -15,29 +15,30 @@ export LC_ALL=C # Silence container locale warning
 # To run scripts for ldpred2 and others one needs to define some directories
 export DIR_BASE=$( git rev-parse --show-toplevel )
 export DIR_SIF=$DIR_BASE/singularity
-export DIR_TESTS=$DIR_BASE/usecases/LDpred2_test
-export DIR_SCRIPTS=$DIR_BASE/usecases/LDpred2
+export DIR_TESTS=$DIR_BASE/tests/test_LDpred2
+export DIR_DATA=$DIR_TESTS/data
+export DIR_SCRIPTS=$DIR_BASE/scripts/pgs/LDpred2
 export DIR_REFERENCE=$DIR_BASE/reference
 # The location of the comorment/ldpred2_ref repository that containts LD matrixes
 export DIR_REF_LDPRED=$DIR_BASE/../ldpred2_ref
 
 # Tutorial data
 # Phenotypic data is part of public-data3.fam file
-export fileInputGeno=$DIR_BASE/usecases/LDpred2_test/data/tutorial_data/public-data3.bed
-export fileInputSumStats=$DIR_BASE/usecases/LDpred2_test/data/tutorial_data/public-data3-sumstats.txt
-export fileOutputSNPR=$DIR_TESTS/data/public-data3.rds
+export fileInputGeno=$DIR_DATA/tutorial_data/public-data3.bed
+export fileInputSumStats=$DIR_DATA/tutorial_data/public-data3-sumstats.txt
+export fileOutputSNPR=$DIR_DATA/public-data3.rds
 export fileKeepSNPS=/REF/hapmap3/w_hm3.justrs
 export fileOut=$DIR_TESTS/output/public-data.score
 
 ### For imputation testing
 # Copy some plink files
-cp $DIR_REFERENCE/examples/prsice2/EUR.bed $DIR_TESTS/data/
-cp $DIR_REFERENCE/examples/prsice2/EUR.bim $DIR_TESTS/data/
-cp $DIR_REFERENCE/examples/prsice2/EUR.fam $DIR_TESTS/data/
+cp $DIR_REFERENCE/examples/prsice2/EUR.bed $DIR_DATA/
+cp $DIR_REFERENCE/examples/prsice2/EUR.bim $DIR_DATA/
+cp $DIR_REFERENCE/examples/prsice2/EUR.fam $DIR_DATA/
 # File to impute
-fileImpute=$DIR_TESTS/data/EUR
+fileImpute=$DIR_DATA/EUR
 # Imputed file
-fileImputed=$DIR_TESTS/data/EUR_imputed
+fileImputed=$DIR_DATA/EUR_imputed
 
 
 # Create shortcut environment variable for Rscript 
