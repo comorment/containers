742da17460e723522f255e489f406dddb6f027da
deepchocolate <andreas@systerdyster.se>
2023-05-15 11:39:33 +0200

    Adapted tests

--- tests/test_LDpred2/scripts/extended.sh
+++ tests/test_LDpred2/scripts/extended.sh
@@ -1,7 +1,7 @@
 LDP="$RSCRIPT $DIR_SCRIPTS/ldpred2.R --file-keep-snps $fileKeepSNPS \
   --merge-by-rsid \
   --col-stat beta --col-stat-se beta_se \
-  --col-snp-id rsid --col-chr chr --col-bp pos --col-A1 a1 --col-A2 a0 \
+  --col-snp-id rsid --col-bp pos --col-A1 a1 --col-A2 a0 \
   --out ${fileOut}_imputed.inf"
 
 # Create sumstats with characters in chromosome column
@@ -11,24 +11,25 @@ row=${row/,22,/,X,}
 fileSumstatsWithChar=$DIR_TESTS/data/sumstats_chr_char.txt
 head -n -1 $fileInputSumStats > $fileSumstatsWithChar
 echo "$row" >> $fileSumstatsWithChar
-dump=$( { $LDP --ldpred-mode inf --geno-file-rds $fileImputed.rds --sumstats $fileSumstatsWithChar; } 2>&1 )
+dump=$( { $LDP --ldpred-mode inf --col-chr chr --geno-file-rds $fileImputed.rds --sumstats $fileSumstatsWithChar; } 2>&1 )
 if [ $? -eq 1 ]; then echo "$dump"; exit; fi
 
 echo "Test error: Missing genotypes"
-dump=$( { $LDP --ldpred-mode inf --geno-file-rds $fileImpute.rds --sumstats $fileInputSumStats; } 2>&1 )
+dump=$( { $LDP --ldpred-mode inf --col-chr chr --geno-file-rds $fileImpute.rds --sumstats $fileInputSumStats; } 2>&1 )
 if [ $? -eq 0 ]; then echo "No error received"; echo "$dump"; exit; fi
 echo "Test --geno-impute-zero"
-dump=$( { $LDP --ldpred-mode inf --geno-file-rds $fileImpute.rds --geno-impute-zero --sumstats $fileInputSumStats; } 2>&1 )
+dump=$( { $LDP --ldpred-mode inf --col-chr chr --geno-file-rds $fileImpute.rds --geno-impute-zero --sumstats $fileInputSumStats; } 2>&1 )
 if [ $? -eq 1 ]; then echo "$dump"; exit; fi
 echo "Test preimputed file"
-dump=$( { $LDP --ldpred-mode inf --geno-file-rds $fileImputed.rds --sumstats $fileInputSumStats; } 2>&1 )
+dump=$( { $LDP --ldpred-mode inf --col-chr chr --geno-file-rds $fileImputed.rds --sumstats $fileInputSumStats; } 2>&1 )
 if [ $? -eq 1 ]; then echo "$dump"; exit; fi
 
 # Note that if files in --dir-genetic-maps do not exist, these will be downloaded. But I think error if the directory doesnt exist
+# --col-chr Chr is to ensure that column names are not case sensitive
 LDP="$RSCRIPT $DIR_SCRIPTS/ldpred2.R --file-keep-snps $fileKeepSNPS \
   --merge-by-rsid \
   --col-stat beta --col-stat-se beta_se \
-  --col-snp-id rsid --col-chr chr --col-bp pos --col-A1 a0 --col-A2 a1 \
+  --col-snp-id rsid --col-chr Chr --col-bp pos --col-A1 a0 --col-A2 a1 \
   --geno-file-rds $fileOutputSNPR --sumstats $fileInputSumStats"
 
 echo "TEST error: Bad ldpred mode"
