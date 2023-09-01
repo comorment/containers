eab1182d26434be978734e2165f1c7d54a5f7530
Espen Hagen <2492641+espenhgn@users.noreply.github.com>
2023-09-01 14:41:28 +0000

    added R2 calcs

--- pgs/Rscripts/evaluate_model.R
+++ pgs/Rscripts/evaluate_model.R
@@ -5,6 +5,7 @@ library(data.table)
 library(stats)
 library(broom)
 library(dplyr)
+library(fmsb)
 
 # deal with parsed arguments
 par <- arg_parser('evaluate PGS model')
@@ -23,13 +24,6 @@ pheno <- fread(parsed$pheno_file, select=c("FID", "IID", parsed$phenotype))
 scores <- fread(parsed$score_file, select=c("FID", "IID", "score"))
 df <- merge(pheno, scores, by=c("FID", "IID"))
 
-# # fit lm model and print summary (not accounting for PCs/covariates)
-# simple.fit = lm(paste(parsed$phenotype, '~.'), 
-#     data=df[,-c("FID", "IID")])
-
-# print(summary(simple.fit))
-# print(glance(simple.fit))
-
 # append columns w. PCs to df
 pcs <- fread(parsed$eigenvec_file, 
              col.names=c("FID", "IID", paste("PC", 1:parsed$nPCs, sep="")), 
@@ -43,18 +37,34 @@ cols <- colnames(df)[5:length(df)]
 
 # fit model
 if (parsed$phenotype_class == "BINARY") {
+    null.formula <- paste(parsed$phenotype, "~", paste(cols, sep="", collapse="+"), sep="", collapse="")  %>% as.formula
+    null.fit <- glm(null.formula, data=df[,-c("FID", "IID")], family="binomial")
     reg.formula <- paste(parsed$phenotype, "~score+", paste(cols, sep="", collapse="+"), sep="", collapse="")  %>% as.formula
     linear.fit <- glm(reg.formula, data=df[,-c("FID", "IID")], family="binomial")
+    # pseudo R2
+    null.eval <- NagelkerkeR2(null.fit)
+    linear.eval <- NagelkerkeR2(linear.fit)
 } else if (parsed$phenotype_class == "CONTINUOUS") {
     null.formula <- paste(parsed$phenotype, "~", paste(cols, sep="", collapse="+"), sep="", collapse="")  %>% as.formula
     null.fit <- lm(null.formula, data=df[,-c("FID", "IID")])
     reg.formula <- paste(parsed$phenotype, "~score+", paste(cols, sep="", collapse="+"), sep="", collapse="")  %>% as.formula
     linear.fit <- lm(reg.formula, data=df[,-c("FID", "IID")])
+    # R2
+    null.eval <- summary(null.fit)$r.squared
+    linear.eval <- summary(linear.fit)$r.squared
 }
 print(summary(null.fit))
 print(glance(null.fit))
 print(summary(linear.fit))
 print(glance(linear.fit))
+
+if (parsed$phenotype_class == "BINARY") {
+    print(paste('Nagelkerke R2 (null model):', null.eval))
+    print(paste('Nagelkerke R2 (full model):', linear.eval))
+} else if {
+    print(paste('R2 (null model):', null.eval))
+    print(paste('R2 (full model):', linear.eval))
+}
 
 # write <out>.txt)
 sink(paste(parsed$out, '.txt', sep=""))
