98b45a17c7554e39062e0fb1a4603198a06e00c1
deepchocolate <andreas@systerdyster.se>
2023-10-18 15:18:43 +0200

    Added coloring

--- tests/test_LDpred2/scripts/functions.sh
+++ tests/test_LDpred2/scripts/functions.sh
@@ -1,18 +1,26 @@
 ### Just some function to simplify testing output codes
 ### Note that warnings are not captured
-# The functions will spit out command output when exit code departs from the expected
+
+function echoRed () {
+	echo -e "\033[0;31m$1\033[0m"
+}
+# These functions will spit out command output when exit code departs from the expected
 # Arg 1: Expected exit code (0 - no error, 1 error)
 # Arg 2: The command to execute as a string
 function testCommand () {
-	dump=$( { $2 ; } 2>&1 )
+	dump=$( { ${@:2} ; } 2>&1 )
 	err=$?
-	if [ ! $err -eq $1 ]; then echo "$dump"; fi
+	if [ ! $err -eq $1 ]; then 
+		echoRed "ERROR ===>"
+		echo "$dump"
+		echoRed "<=== END ERROR"
+	fi
 }
 # Test successful command
 function testSuccess () {
-	testCommand 0 "$1"
+	testCommand 0 ${@:1}
 }
 # Test failing command
 function testException () {
-	testCommand 1 "$1"
+	testCommand 1 ${2:@}
 }
