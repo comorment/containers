a53654ac596f5e779e2ba8fbfaa9ccf27a0f4d74
Espen Hagen <2492641+espenhgn@users.noreply.github.com>
2024-01-31 14:08:56 +0000

    Include samtools package in gwas.sif Fixes #219

--- docker/scripts/install_bcftools.sh
+++ docker/scripts/install_bcftools.sh
@@ -3,13 +3,13 @@ set -euo pipefail
 
 # deps 
 apt-get update && apt-get install -y --no-install-recommends \
-    libcurl4-gnutls-dev=7.68.0-1ubuntu2.19 \
-    libperl-dev=5.30.0-9ubuntu0.4 \
+    libcurl4-gnutls-dev=7.68.0-1ubuntu2.21 \
+    libperl-dev=5.30.0-9ubuntu0.5 \
     && \
     apt-get clean && \
     rm -rf /var/lib/apt/lists/*
 
-# bcfools
+# bcftools
 git clone --recurse-submodules --depth 1 -b 1.12 https://github.com/samtools/bcftools.git && \
 cd bcftools && \
 autoheader && autoconf && ./configure --enable-libgsl --enable-perl-filters --with-htslib=/usr/ && \
--- docker/scripts/install_samtools.sh
+++ docker/scripts/install_samtools.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 set -euo pipefail
 
-# bcfools
+apt-get update && apt-get install -y --no-install-recommends \
+    libncurses-dev=6.2-0ubuntu2.1 \
+    && \
+    apt-get clean && \
+    rm -rf /var/lib/apt/lists/*
+
+# samtools
 git clone --depth 1 -b 1.12 https://github.com/samtools/samtools.git && \
-cd bcftools && \
+cd samtools && \
     autoheader && \
     autoconf -Wno-syntax && \
     ./configure --with-htslib=/usr/ && \
