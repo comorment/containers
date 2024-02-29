b277053a308094d5711e480451e1ae9e648b58fe
ttfiliz <110425985+ttfiliz@users.noreply.github.com>
2024-02-29 15:13:44 +0100

    Added fix for #232

--- scripts/gwas/gwas.py
+++ scripts/gwas/gwas.py
@@ -1128,7 +1128,7 @@ def append_job(args, commands, array_spec, slurm_job_index, cmd_file, submit_job
 def rename_iid_column(log, pheno_dict, pheno):
     if np.sum(pheno_dict['TYPE'] == 'IID') != 1:
         raise ValueError('Exacly one column in the dictionary file must be marked as IID')
-    iid_column_name = pheno_dict.loc[pheno_dict['TYPE'] == 'IID', 'FIELD'][0]
+    iid_column_name = pheno_dict.loc[pheno_dict['TYPE'] == 'IID', 'FIELD'].values[0]
     if iid_column_name not in pheno.columns:
         raise ValueError(f'IID column ({iid_column_name}) not present in --pheno-file')
     if (iid_column_name != 'IID') and ('IID' in pheno.columns):
