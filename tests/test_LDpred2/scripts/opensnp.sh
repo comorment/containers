f6fb5a5ed1b2a8f2f8fd73ea95788abab0615809
deepchocolate <andreas@systerdyster.se>
2024-03-05 08:51:34 +0100

    Added script for opensnp data

---
+++ tests/test_LDpred2/scripts/opensnp.sh
@@ -0,0 +1,44 @@
+# Set environmental variables. Replace "<path/to/comorment>" with 
+# the full path to the folder containing cloned "containers" and "ldpred2_ref" repositories
+export COMORMENT=$DIR_COMORMENT
+export SIF=$COMORMENT/containers/singularity
+export REFERENCE=$COMORMENT/containers/reference
+export LDPRED2_REF=$COMORMENT/ldpred2_ref
+export OPENSNP=$COMORMENT/opensnp
+export SINGULARITY_BIND=$REFERENCE:/REF,${LDPRED2_REF}:/ldpred2_ref,${OPENSNP}:/opensnp,${COMORMENT}:/comorment
+
+# Point to LDpred2.R input/output files
+export fileGeno=/opensnp/imputed/opensnp_hm3.bed
+export fileGenoRDS=opensnp_hm3.rds
+export fileSumstats=/opensnp/gwas/UKB_NEALELAB_2018_HEIGHT.GRCh37.hm3.gz
+export fileOut=Height
+export dirLDpred2=/comorment/containers/scripts/pgs/LDpred2
+
+export RSCRIPT="singularity exec --home=$PWD:/home $SIF/r.sif Rscript"
+
+# convert genotype to LDpred2 format
+$RSCRIPT $dirLDpred2/createBackingFile.R --file-input $fileGeno --file-output $fileGenoRDS
+
+# impute
+$RSCRIPT $dirLDpred2/imputeGenotypes.R --impute-simple mean0 --geno-file-rds $fileGenoRDS
+
+# Generate PGS usign LDPRED-inf
+$RSCRIPT $dirLDpred2/ldpred2.R \
+ --ldpred-mode inf \
+ --col-stat OR \
+ --col-stat-se SE \
+ --stat-type OR \
+ --geno-file-rds $fileGenoRDS \
+ --sumstats $fileSumstats \
+ --out $fileOut.inf
+
+# Generate PGS using LDPRED2-auto
+$RSCRIPT $dirLDpred2/ldpred2.R \
+ --ldpred-mode auto \
+ --col-stat OR \
+ --col-stat-se SE \
+ --stat-type OR \
+ --geno-file-rds $fileGenoRDS \
+ --sumstats $fileSumstats \
+ --out $fileOut.auto
+ 
