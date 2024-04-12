4f8ff8f725bc6e9707b4d13de0527d8b3731d5f2
Espen Hagen <2492641+espenhgn@users.noreply.github.com>
2024-04-12 16:40:53 +0200

    Build container image `hello:latest` using build action  (#252)
    * Push to Docker registries
    Fixes #251
    
    * update changelog; add installer script to workflow
    
    * installer scripts
    
    * missing context?
    
    * another context
    
    * update deb packages
    
    * test
    
    * use only GITHUB_TOKEN
    
    * test run on main branch

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -21,6 +21,7 @@ If MD5 sum is not listed for a certain release then it means that the container 
 
 ### Added
 
+* Added workflow for building containers on GitHub Actions and pushing them to GitHub Container Registry
 * Added options `--extract`, `--extract-step1`, `--extract-step2`, `--exclude`, `--exclude-step1`, and `--exclude-step2` to the `gwas.py` script to enable inclusion and exclusion of SNPs
 * Added Rstudio-server and R packages info to `r.sif` container documentation
 
--- docker/scripts/apt_get_essential.sh
+++ docker/scripts/apt_get_essential.sh
@@ -12,17 +12,17 @@ apt-get update && apt-get install -y --no-install-recommends \
    build-essential=12.8ubuntu1 \
    bzip2=1.0.8-2 \
    cmake=3.16.3-1ubuntu1.20.04.1 \
-   curl=7.68.0-1ubuntu2.21 \
+   curl=7.68.0-1ubuntu2.22 \
    dos2unix=7.4.0-2 \
    gdb=9.1-0ubuntu1 \
    gfortran=4:9.3.0-1ubuntu2 \
    git=1:2.25.1-1ubuntu3.11 \
-   less=551-1ubuntu0.1 \
+   less=551-1ubuntu0.2 \
    libatlas-base-dev=3.10.3-8ubuntu7 \
-   libcurl4-openssl-dev=7.68.0-1ubuntu2.21 \
+   libcurl4-openssl-dev=7.68.0-1ubuntu2.22 \
    libgomp1=10.5.0-1ubuntu1~20.04 \
    libgsl-dev=2.5+dfsg-6+deb10u1build0.20.04.1 \
-   libnss3=2:3.49.1-1ubuntu1.9 \
+   libnss3=2:3.98-0ubuntu0.20.04.2 \
    libpcre2-dev=10.34-7ubuntu0.1 \
    libxt-dev=1:1.1.5-1 \
    pandoc=2.5-3build2 \
@@ -34,7 +34,7 @@ apt-get update && apt-get install -y --no-install-recommends \
    tar=1.30+dfsg-7ubuntu0.20.04.4 \
    tofrodos=1.7.13+ds-4 \
    unzip=6.0-25ubuntu1.1 \
-   vim=2:8.1.2269-1ubuntu5.21 \
+   vim=2:8.1.2269-1ubuntu5.22 \
    wget=1.20.3-1ubuntu2 \
    zlib1g-dev=1:1.2.11.dfsg-2ubuntu1.5 \
    && \
