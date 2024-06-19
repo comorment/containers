d956c85a97adca1ee3afe6ba32ff873067e95bb9
Espen Hagen <2492641+espenhgn@users.noreply.github.com>
2024-06-19 09:11:03 +0200

    GITHUB_PAT

--- docker/scripts/R/github.R
+++ docker/scripts/R/github.R
@@ -1,7 +1,7 @@
 url <- "https://packagemanager.posit.co/cran/__linux__/focal/2023-02-16"
 dependencies <- c('Depends', 'Imports', 'LinkingTo')
 upgrade <- 'default'
-auth_token <- Sys.getenv("github_token")
+auth_token <- Sys.getenv("GITHUB_PAT")
 
 # GitHub packages w. Git SHA
 packages <- list(
