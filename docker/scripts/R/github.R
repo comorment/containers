76524091631bc774e1a957d886245f67c051b7c3
juuf <47811956+juuf@users.noreply.github.com>
2024-06-26 15:41:20 +0200

    Update gSEM Git SHA

--- docker/scripts/R/github.R
+++ docker/scripts/R/github.R
@@ -9,7 +9,7 @@ packages <- list(
     'alexploner/cfdr.pleio' = '76d5085e6d3f3ca9576d5d7564d2acf11bcfd021',
     'dajiangliu/rareGWAMA' = '72e962dae19dc07251244f6c33275ada189c2126',
     'deepchocolate/glm-extras' = '91f8d53c886b27b7c9941df6c3233f99981323a8',
-    'GenomicSEM/GenomicSEM' = 'bcbbaffff5767acfc5c020409a4dc54fbf07876b',
+    'GenomicSEM/GenomicSEM' = 'd3ddccc2825228cde27a70f155cdbcde9ebcdf68',
     'jamesliley/cfdr' = 'ec5fddbd27c746a470eb827dc249a80194b231e8',
     'jamesliley/cFDR-common-controls' = '9b923fea283e2373ee8effa2909620a1930004bd',
     # gwasvcf deps:
