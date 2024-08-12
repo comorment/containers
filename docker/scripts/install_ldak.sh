8db2160ff70171f9ca1f3641e122af6b2a2e404d
Espen Hagen <2492641+espenhgn@users.noreply.github.com>
2024-08-12 14:34:15 +0000

    LDAK git-sha

--- docker/scripts/install_ldak.sh
+++ docker/scripts/install_ldak.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -euo pipefail
 
-wget https://github.com/dougspeed/LDAK/blob/main/ldak6.linux
+wget https://github.com/dougspeed/LDAK/blob/4ee871be17d8ea406494211638a5ead677e7dd47/ldak6.linux
 cp ldak6.linux /bin/ldak
