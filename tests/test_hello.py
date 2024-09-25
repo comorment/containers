0b2df0b9442d296b708b426af02821dc1dc8d242
Espen Hagen <2492641+espenhgn@users.noreply.github.com>
2024-09-25 17:43:57 +0200

    don't set docker port

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -5,13 +5,7 @@ Test module for ``hello.sif`` build
 """
 
 import os
-import socket
 import subprocess
-
-# port used by tests
-sock = socket.socket()
-sock.bind(('', 0))
-port = sock.getsockname()[1]
 
 # Check that (1) singularity exist, and (2) if not, check for docker.
 # If neither are found, tests will fail
@@ -23,7 +17,7 @@ try:
 except FileNotFoundError:
     try:
         subprocess.run('docker', check=False)
-        PREFIX = (f'docker run -p {port}:{port} ' +
+        PREFIX = ('docker run ' +
                   'ghcr.io/comorment/hello')
         PLINK = f'{PREFIX} plink'
     except FileNotFoundError:
