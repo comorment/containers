b71970fd5e35293a0a68a0e40a4d7da7e5f6d13c
Espen Hagen <2492641+espenhgn@users.noreply.github.com>
2024-09-25 17:08:19 +0200

    add test for hello build

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -5,11 +5,33 @@ Test module for ``hello.sif`` build
 """
 
 import os
+import socket
 import subprocess
 
+# port used by tests
+sock = socket.socket()
+sock.bind(('', 0))
+port = sock.getsockname()[1]
+
+# Check that (1) singularity exist, and (2) if not, check for docker.
+# If neither are found, tests will fail
+try:
+    pth = os.path.join('singularity', 'hello.sif')
+    subprocess.run('singularity', check=False)
+    PREFIX = f'singularity run {pth}'
+    PLINK = f'{PREFIX} plink'
+except FileNotFoundError:
+    try:
+        subprocess.run('docker', check=False)
+        PREFIX = (f'docker run -p {port}:{port} ' +
+                  'ghcr.io/comorment/hello')
+        PLINK = f'{PREFIX} plink'
+    except FileNotFoundError:
+        # neither singularity nor docker found, fall back to plain python
+        # presumably because we are running on the client
+        PLINK = 'plink'
 
 def test_hello_plink():
-    pth = os.path.join('singularity', 'hello.sif')
-    call = f'singularity run {pth} plink --version'
-    out = subprocess.run(call.split(' '))
+    call = f'{PLINK} --version'
+    out = subprocess.run(call.split(' '), check=False)
     assert out.returncode == 0
