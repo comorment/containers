bf381461ff11fb7d9771e59d021796cb95bbec6f
Espen Hagen <2492641+espenhgn@users.noreply.github.com>
2024-10-14 11:57:33 +0200

    revise bioconductor install script

--- docker/scripts/R/bioconductor.R
+++ docker/scripts/R/bioconductor.R
@@ -1,3 +1,15 @@
+# set up
+dependencies <- c('Depends', 'Imports', 'LinkingTo')
+upgrade <- 'default'
+
+url = "https://packagemanager.posit.co/bioconductor/__linux__/jammy/2024-10-01"
+# Configure BioCManager to use Posit Package Manager:
+options(BioC_mirror = "https://packagemanager.posit.co/bioconductor/2024-10-01")
+options(BIOCONDUCTOR_CONFIG_FILE = "https://packagemanager.posit.co/bioconductor/2024-10-01/config.yaml")
+
+# Configure a CRAN snapshot compatible with Bioconductor 3.20:
+options(repos = c(CRAN = "https://packagemanager.posit.co/cran/__linux__/jammy/2024-10-01"))
+
 # Bioconductor packages
 packages <- c(
     'AnnotationDbi',
@@ -16,10 +28,12 @@ packages <- c(
     'zlibbioc')
 
 # install package from Bioconductor and quit with error if installation fails
+library(devtools)
 for (package in packages) {
     tryCatch(
     {
-        BiocManager::install(package, version='3.12')
+        devtools::install_bioc(package, mirror=url, dependencies=dependencies, upgrade=upgrade)
+        # BiocManager::install(package, version='3.12')
     },
     error = function(e) {
         cat("Error occurred during package installation:\n")
