2f34c0df11eed0bc9a34125d014d06f52462ca0a
Espen Hagen <2492641+espenhgn@users.noreply.github.com>
2025-02-19 13:54:53 +0100

    lint

--- containers/pull_all_containers.py
+++ containers/pull_all_containers.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# script to pull/build singularity image format files from 
-# pushed registry builds. 
+# script to pull/build singularity image format files from
+# pushed registry builds.
 
 import os
 import tempfile
@@ -9,12 +9,14 @@ import shutil
 import argparse
 from packaging.version import Version, InvalidVersion
 
-# Manually curated list of containers to pull. 
-# Defining things manually as some registries may have many tags and we don't want them all.
+# Manually curated list of containers to pull.
+# Defining things manually as some registries may have many tags and we
+# don't want them all.
 # We may also choose to sync only registry pulls without git repos/codes
 
 # parse input tags
-parser = argparse.ArgumentParser(description='Pull all containers from registry')
+parser = argparse.ArgumentParser(
+    description='Pull all containers from registry')
 parser.add_argument('tags', nargs='+', help='list of tags to pull')
 tags = parser.parse_args().tags
 
@@ -24,26 +26,26 @@ docker_packages_dest_prefix_tags = {
         'prefix': 'hello',  # file prefix
         'tags': tags,  # list of tags
         'create_tag_dir': True   # create target directory for each tag
-        },
+    },
     'ghcr.io/comorment/gwas': {
         'dest': os.getcwd(),
         'prefix': 'gwas',
         'tags': tags,
         'create_tag_dir': True
-        },
+    },
     'ghcr.io/comorment/python3': {
         'dest': os.getcwd(),
         'prefix': 'python3',
         'tags': tags,
         'create_tag_dir': True
-        },
+    },
     'ghcr.io/comorment/r': {
         'dest': os.getcwd(),
         'prefix': 'r',
         'tags': tags,
         'create_tag_dir': True
-        },
-    }
+    },
+}
 
 # recursively pull and create .sif files if they don't exist:
 for ref, dest_prefix_tags in docker_packages_dest_prefix_tags.items():
@@ -51,7 +53,7 @@ for ref, dest_prefix_tags in docker_packages_dest_prefix_tags.items():
     prefix = dest_prefix_tags['prefix']
     tags = dest_prefix_tags['tags']
     create_tag_dir = dest_prefix_tags['create_tag_dir']
-    
+
     # make destination directory
     os.makedirs(dest, exist_ok=True)
 
@@ -66,37 +68,42 @@ for ref, dest_prefix_tags in docker_packages_dest_prefix_tags.items():
             try:
                 assert shutil.which('oras') is not None
                 print(f'pulling build {fname}')
-                os.system(f'oras pull -o {tag} {ref}_sif:{tag}')  # note "_sif" suffix
+                # note "_sif" suffix
+                os.system(f'oras pull -o {tag} {ref}_sif:{tag}')
             except AssertionError:
                 try:
                     shutil.which('apptainer') is not None
                 except AssertionError:
-                    mssg = 'Neither oras nor apptainer executables found. Please install one of them.'
+                    mssg = 'Neither oras nor apptainer executables found.'
                     raise Exception(mssg)
                 print(f'creating build {fname}:')
                 with tempfile.TemporaryDirectory() as tmpdir:
-                    os.system(f'export APPTAINER_CACHEDIR={tmpdir} && apptainer pull {fname} docker://{ref}:{tag}')
-    
+                    cmmd = f'''export APPTAINER_CACHEDIR={tmpdir} && \n
+                    apptainer pull {fname} docker://{ref}:{tag}'''
+                    os.system(
+                        )
+
     # if 'latest' not in tags, symlink tag with highest semantic version:
     cwd = os.getcwd()
-    if not 'latest' in tags:
+    if 'latest' not in tags:
         try:
             max_version = max([Version(t) for t in tags])
             for tag in tags:
                 if Version(tag) == max_version:
                     break
-        except InvalidVersion as iv:
+        except InvalidVersion:
             # deal with unsupported tag formats
             max_version = max([Version(t.split('-')[0]) for t in tags])
             for tag in tags:
                 if tag.split('-')[0] == str(max_version):
                     break
-        
+
         os.chdir(dest)
         if create_tag_dir:
             # symlink tag dir as latest
             os.system(f"ln -s -f -n {tag} latest")
         else:
             # symlink tagged file as latest
-            os.system(f"ln -s -f {f'{prefix}_{tag}.sif'} {f'{prefix}_latest.sif'}")
+            os.system(
+                f"ln -s -f {f'{prefix}_{tag}.sif'} {f'{prefix}_latest.sif'}")
         os.chdir(cwd)
