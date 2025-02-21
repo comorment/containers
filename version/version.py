23f139d747d7ca438aeec0d07fdd33e401cc9807
Espen Hagen <2492641+espenhgn@users.noreply.github.com>
2025-02-21 08:14:14 +0100

    test bump old scikit-learn, sksurv versions (#300)
    * test bump old scikit-learn, sksurv versions
    
    * bump libssl-dev

--- version/version.py
+++ version/version.py
@@ -5,7 +5,7 @@ _MINOR = "9"
 _PATCH = "0"
 # This is mainly for nightly builds which have the suffix ".dev$DATE". See
 # https://semver.org/#is-v123-a-semantic-version for the semantics.
-_SUFFIX = ""
+_SUFFIX = "dev"
 
 VERSION_SHORT = "{0}.{1}".format(_MAJOR, _MINOR)
 VERSION = "{0}.{1}.{2}{3}".format(_MAJOR, _MINOR, _PATCH, _SUFFIX)
